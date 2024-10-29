cea42085fa1377ce66ed560e3e794791d94f609a
mechanicMo <mohitr35@gmail.com>
2024-10-29 15:37:28 -0700

    v1 of adding a new node, scales well but initial position could use improvement

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -4,23 +4,32 @@ import ReactFlow, {
   MarkerType,
   useEdgesState,
   useNodesState,
+  type Connection,
+  type Edge,
+  type Node,
 } from "reactflow";
 
 import "reactflow/dist/style.css";
 
+let nodeId = 0;
+const getNextId = () => {
+  nodeId++;
+  return nodeId.toString();
+};
+
 const initialNodes = [
   {
-    id: "1",
+    id: getNextId(),
     position: { x: 0, y: 0 },
     data: { label: "1" },
   },
   {
-    id: "2",
+    id: getNextId(),
     position: { x: 0, y: 100 },
     data: { label: "2" },
   },
   {
-    id: "3",
+    id: getNextId(),
     position: { x: 0, y: 200 },
     data: { label: "3" },
   },
@@ -44,17 +53,34 @@ const initialEdges = [
   },
 ];
 
+const createNewNode = (): Node => {
+  const id = getNextId();
+
+  return {
+    id,
+    position: { x: 0, y: 0 },
+    data: { label: id },
+  };
+};
+
 export const Chart = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
 
+  const addNewNode = () => {
+    const node = createNewNode();
+    setNodes((nds) => [...nds, node]);
+  };
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
+      <button onClick={addNewNode}>click to add a new node</button>
+
       <ReactFlow
         nodes={nodes}
         edges={edges}
