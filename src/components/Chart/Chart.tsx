49fbbd3d97959288f85e22b265863dc3578b8027
mechanicMo <mohitr35@gmail.com>
2024-10-29 12:24:45 -0700

    add interactivity

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -1,48 +1,67 @@
-import React from 'react';
-import ReactFlow, { MarkerType } from 'reactflow';
+import React, { useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  MarkerType,
+  useEdgesState,
+  useNodesState,
+} from "reactflow";
 
-import 'reactflow/dist/style.css';
+import "reactflow/dist/style.css";
 
 const initialNodes = [
-    {
-        id: '1',
-        position: { x: 0, y: 0 },
-        data: { label: '1' }
-    },
-    {
-        id: '2',
-        position: { x: 0, y: 100 },
-        data: { label: '2' }
-    },
-    {
-        id: '3',
-        position: { x: 0, y: 200 },
-        data: { label: '3' }
-    },
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "1" },
+  },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    data: { label: "2" },
+  },
+  {
+    id: "3",
+    position: { x: 0, y: 200 },
+    data: { label: "3" },
+  },
 ];
 const initialEdges = [
-    {
-        id: 'e1-2',
-        source: '1',
-        target: '2',
-        markerend: {
-          type: MarkerType.ArrowClosed,
-         }
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+    markerend: {
+      type: MarkerType.ArrowClosed,
     },
-    {
-        id: 'e2-3',
-        source: '2',
-        target: '3',
-        markerend: {
-          type: MarkerType.ArrowClosed,
-         }
+  },
+  {
+    id: "e2-3",
+    source: "2",
+    target: "3",
+    markerend: {
+      type: MarkerType.ArrowClosed,
     },
+  },
 ];
 
 export const Chart = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+      />
     </div>
   );
-}
+};
